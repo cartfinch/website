89106f16431e32e27700e4d99aff5cc4decaf3e6
CartFinch <cartfinch@gmail.com>
2026-05-13 19:47:05 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // You might need to install 'path' or use 'import { fileURLToPath } from "url"'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' for custom domains like cartfinch.com
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: '/', 
 })
